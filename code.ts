2bcc6bcd443eb4279e437445545638cd81626ff1
Roman Nebel <im.roman.nebel@gmail.com>
2024-01-17 21:48:16 +0100

    Add shapes counter Update UI

--- code.ts
+++ code.ts
@@ -1,10 +1,10 @@
-const UIOptions = { width: 300, height: 210 };
+const UIOptions = { width: 300, height: 495 };
 
 figma.showUI(__html__, UIOptions);
 
 type UIMessage = {
-  action: "SEND_FRAME_COUNTER"
-  counter?: number
+  action: "SEND_COUNTERS"
+  data: Object
 }
 
 function SendToUI(msg: UIMessage) {
@@ -12,19 +12,28 @@ function SendToUI(msg: UIMessage) {
 }
 
 function getDefaultNamedLayers() {
-  const layers = figma.currentPage.findAllWithCriteria({ types: ['FRAME'] })
-  const defaultLayers = layers.filter((layer) => layer.name.match(/Frame [0-9]+/))
+  const frames = figma.currentPage.findAllWithCriteria({ types: ['FRAME'] })
+  const defaultFrames = frames.filter((frame) => frame.name.match(/Frame [0-9]+/))
+
+  const shapes = figma.currentPage.findAllWithCriteria({ types: ['RECTANGLE', 'LINE', 'VECTOR', 'ELLIPSE', 'POLYGON', 'STAR'] })
+  const defaultShapes = shapes.filter((shape) => shape.name.match(/Rectangle\s[0-9]+|Line\s[0-9]+|Arrow\s[0-9]+|Ellipse\s[0-9]+|Polygon\s[0-9]+|Star\s[0-9]+/))
+
+  console.log(defaultShapes.length)
 
   SendToUI({
-    action: "SEND_FRAME_COUNTER",
-    counter: defaultLayers.length
+    action: "SEND_COUNTERS",
+    data: {
+      totalFrames: frames.length,
+      defaultFrames: defaultFrames.length,
+      totalShapes: shapes.length,
+      defaultShapes: defaultShapes.length
+    }
+    
   })
 }
 
 figma.ui.onmessage = msg => {
-  if (msg.action === 'UPDATE_FRAME_COUNTER') {
+  if (msg.action === 'UPDATE_COUNTERS') {
     getDefaultNamedLayers()
   }
 };
-
-getDefaultNamedLayers()
