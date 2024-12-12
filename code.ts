0131101d9a4d553c33c45a43b31699bd070862a0
Roman Nebel <im.roman.nebel@gmail.com>
2024-12-13 00:20:42 +0100

    Fix string optimisation

--- code.ts
+++ code.ts
@@ -1,36 +1,51 @@
 type PageTypes = {
-  page: "index" | "rename"
-}
+  page: "index" | "rename";
+};
 
-type PrimitiveNodes = Array<'RECTANGLE' | 'LINE' | 'VECTOR' | 'ELLIPSE' | 'POLYGON' | 'STAR'>
+type PrimitiveNodes = Array<
+  "RECTANGLE" | "LINE" | "VECTOR" | "ELLIPSE" | "POLYGON" | "STAR"
+>;
 
-const UIOptions = 
-  {
-    index: { width: 300, height: 680 },
-    rename: { width: 300, height: 200 }
-  };
+const UIOptions = {
+  index: { width: 300, height: 680 },
+  rename: { width: 300, height: 200 },
+};
 
 figma.showUI(__uiFiles__.index, UIOptions.index);
 
-const PRIMITIVES: PrimitiveNodes = ['RECTANGLE', 'LINE', 'VECTOR', 'ELLIPSE', 'POLYGON', 'STAR']
+const PRIMITIVES: PrimitiveNodes = [
+  "RECTANGLE",
+  "LINE",
+  "VECTOR",
+  "ELLIPSE",
+  "POLYGON",
+  "STAR",
+];
 
-let initState = true
+let initState = true;
 
-let allNodes: any = []
-let framesCount: number | null = null
-let defaultFrames: FrameNode[] = []
-let emptyFrames: FrameNode[] = []
-let shapesCount: number | null = null
-let defaultShapes: (RectangleNode | LineNode | VectorNode | EllipseNode | PolygonNode | StarNode)[] = []
-let instancesCount: number | null = null
-let defaultInstances: InstanceNode[] = []
-let badNamedNodes: any = []
+let allNodes: any = [];
+let framesCount: number | null = null;
+let defaultFrames: FrameNode[] = [];
+let emptyFrames: FrameNode[] = [];
+let shapesCount: number | null = null;
+let defaultShapes: (
+  | RectangleNode
+  | LineNode
+  | VectorNode
+  | EllipseNode
+  | PolygonNode
+  | StarNode
+)[] = [];
+let instancesCount: number | null = null;
+let defaultInstances: InstanceNode[] = [];
+let badNamedNodes: any = [];
 
 type UIMessage = {
-  action: "SEND_COUNTERS" | "SEND_NAME"
-  data: Object
-  state?: boolean
-}
+  action: "SEND_COUNTERS" | "SEND_NAME";
+  data: Object;
+  state?: boolean;
+};
 
 function isUpperCase(str: string) {
   return str === str.toUpperCase();
@@ -42,7 +57,7 @@ function isLowerCase(str: string) {
 
 function findIndexes(str: string, regex: RegExp) {
   var indexes: number[] = [];
-  str.replace(regex, function(match, index) {
+  str.replace(regex, function (match, index) {
     indexes.push(index);
     return match;
   });
@@ -64,39 +79,55 @@ function replaceCharAtIndex(str: string, index: number) {
 }
 
 function SendToUI(msg: UIMessage) {
-  figma.ui.postMessage(msg)
+  figma.ui.postMessage(msg);
 }
 
 function checkName(type: any, newName: string) {
   if (type === "FRAME") {
-    const re = new RegExp("Frame [0-9]+")
-    return !re.test(newName)
-  } 
-  
-  if (PRIMITIVES.includes(type)) {
-    const re = new RegExp("Rectangle\s[0-9]+|Line\s[0-9]+|Arrow\s[0-9]+|Ellipse\s[0-9]+|Polygon\s[0-9]+|Star\s[0-9]+")
-    return !re.test(newName)
+    const re = new RegExp("Frame [0-9]+");
+    return !re.test(newName);
   }
 
-  return true
+  if (PRIMITIVES.includes(type)) {
+    const re = new RegExp(
+      "Rectangles[0-9]+|Lines[0-9]+|Arrows[0-9]+|Ellipses[0-9]+|Polygons[0-9]+|Stars[0-9]+"
+    );
+    return !re.test(newName);
+  }
+
+  return true;
 }
 
 function getDefaultNamedLayers() {
-  allNodes = figma.currentPage.findAllWithCriteria({ types: ['FRAME', 'INSTANCE', ...PRIMITIVES] })
+  allNodes = figma.currentPage.findAllWithCriteria({
+    types: ["FRAME", "INSTANCE", ...PRIMITIVES],
+  });
 
-  const frames: any = allNodes.filter((node: any) => node.type === 'FRAME')
-  framesCount = frames.length
+  const frames: any = allNodes.filter((node: any) => node.type === "FRAME");
+  framesCount = frames.length;
 
-  defaultFrames = frames.filter((frame: any) => frame.name.match(/Frame [0-9]+/))
-  emptyFrames = frames.filter((frame: any) => frame.children.length === 0)
+  defaultFrames = frames.filter((frame: any) =>
+    frame.name.match(/Frame [0-9]+/)
+  );
+  emptyFrames = frames.filter((frame: any) => frame.children.length === 0);
 
-  const shapes: any = allNodes.filter((node: any) => PRIMITIVES.includes(node.type))
-  shapesCount = shapes.length
-  defaultShapes = shapes.filter((shape: any) => shape.name.match(/Rectangle\s[0-9]+|Line\s[0-9]+|Arrow\s[0-9]+|Ellipse\s[0-9]+|Polygon\s[0-9]+|Star\s[0-9]+/))
+  const shapes: any = allNodes.filter((node: any) =>
+    PRIMITIVES.includes(node.type)
+  );
+  shapesCount = shapes.length;
+  defaultShapes = shapes.filter((shape: any) =>
+    shape.name.match(
+      /Rectangle\s[0-9]+|Line\s[0-9]+|Arrow\s[0-9]+|Ellipse\s[0-9]+|Polygon\s[0-9]+|Star\s[0-9]+/
+    )
+  );
 
-  const instances: any = allNodes.filter((node: any) => node.type === 'INSTANCE')
-  instancesCount = instances.length
-  defaultInstances = instances.filter((node: any) => node.name === node?.mainComponent?.name)
+  const instances: any = allNodes.filter(
+    (node: any) => node.type === "INSTANCE"
+  );
+  instancesCount = instances.length;
+  defaultInstances = instances.filter(
+    (node: any) => node.name === node?.mainComponent?.name
+  );
 
   return {
     totalFrames: framesCount,
@@ -105,8 +136,8 @@ function getDefaultNamedLayers() {
     totalShapes: shapesCount,
     defaultShapes: defaultShapes.length,
     totalInstances: instancesCount,
-    defaultInstances: defaultInstances.length
-  }
+    defaultInstances: defaultInstances.length,
+  };
 }
 
 function getBadNamedLayers(convention: any) {
@@ -116,33 +147,34 @@ function getBadNamedLayers(convention: any) {
     (node: any) => new RegExp(" |__|-|--").test(node.name),
     (node: any) => new RegExp(" |_|-|--").test(node.name),
     (node: any) => new RegExp(" |_|__|--").test(node.name),
-    (node: any) => new RegExp(" |_|__|-").test(node.name)
-  ]
-  badNamedNodes = allNodes.filter(rules[Number(convention)-1])
+    (node: any) => new RegExp(" |_|__|-").test(node.name),
+  ];
+  badNamedNodes = allNodes.filter(rules[Number(convention) - 1]);
   return {
-    badNamedNodes: badNamedNodes.length
-  }
+    badNamedNodes: badNamedNodes.length,
+  };
 }
 
-figma.ui.onmessage = msg => {
-  const { action } = msg
-  if (action === 'UPDATE_COUNTERS') {
-    const { convention } = msg
-    const defaulNamed = getDefaultNamedLayers()
-    const badNamed = getBadNamedLayers(convention)
+figma.ui.onmessage = (msg) => {
+  const { action } = msg;
+  if (action === "UPDATE_COUNTERS") {
+    const { convention } = msg;
+    const defaulNamed = getDefaultNamedLayers();
+    const badNamed = getBadNamedLayers(convention);
 
-    initState = false
+    initState = false;
 
     SendToUI({
       action: "SEND_COUNTERS",
       data: {
-        ...defaulNamed, ...badNamed
+        ...defaulNamed,
+        ...badNamed,
       },
-      state: initState
-    })
+      state: initState,
+    });
   }
 
-  if (action === 'BACKGROUND_UPDATE_COUNTERS') {
+  if (action === "BACKGROUND_UPDATE_COUNTERS") {
     SendToUI({
       action: "SEND_COUNTERS",
       data: {
@@ -153,22 +185,22 @@ figma.ui.onmessage = msg => {
         defaultShapes: defaultShapes.length,
         totalInstances: instancesCount,
         defaultInstances: defaultInstances.length,
-        badNamedNodes: badNamedNodes.length
+        badNamedNodes: badNamedNodes.length,
       },
-      state: initState
-    })
+      state: initState,
+    });
   }
 
-  if (action === 'CHANGE_PAGE') {
-    const { page }: PageTypes = msg
+  if (action === "CHANGE_PAGE") {
+    const { page }: PageTypes = msg;
     figma.showUI(__uiFiles__[page], UIOptions[page]);
   }
 
-  if (action === 'DELETE_EMPTY') {
+  if (action === "DELETE_EMPTY") {
     for (const frame of emptyFrames) {
-      frame.remove()
+      frame.remove();
     }
-    emptyFrames = []
+    emptyFrames = [];
     SendToUI({
       action: "SEND_COUNTERS",
       data: {
@@ -179,38 +211,38 @@ figma.ui.onmessage = msg => {
         defaultShapes: defaultShapes.length,
         totalInstances: instancesCount,
         defaultInstances: defaultInstances.length,
-        badNamedNodes: badNamedNodes.length
-      }
-    })
+        badNamedNodes: badNamedNodes.length,
+      },
+    });
   }
 
-  if (action === 'FIX_NAMING') {
-    const { convention } = msg
-    const symbols = ["", "", "_", "__", "-", "--"]
+  if (action === "FIX_NAMING") {
+    const { convention } = msg;
+    const symbols = ["", "", "_", "__", "-", "--"];
     for (const frame of badNamedNodes) {
       if (convention === "1") {
-        frame.name[0].toLowerCase()
+        frame.name = frame.name[0].toLowerCase() + frame.name.slice(1);
       }
       if (convention === "2") {
-        frame.name[0].toUpperCase()
+        frame.name = frame.name[0].toUpperCase() + frame.name.slice(1);
       }
       if (convention === "1" || convention === "2") {
-        const indexes = findIndexes(frame.name, new RegExp(" |_|__|-|--"))
-        let newName = frame.name
-        console.log(newName, indexes)
+        const indexes = findIndexes(frame.name, new RegExp(" |_|__|-|--"));
+        let newName = frame.name;
+        console.log(newName, indexes);
         for (const i of indexes) {
-          newName = replaceCharAtIndex(newName, i+1)
+          newName = replaceCharAtIndex(newName, i + 1);
         }
-        frame.name = newName
+        frame.name = newName;
       }
       frame.name = frame.name
-        .replaceAll(" ", symbols[convention-1])
-        .replaceAll("_", symbols[convention-1])
-        .replaceAll("__", symbols[convention-1])
-        .replaceAll("-", symbols[convention-1])
-        .replaceAll("--", symbols[convention-1])
+        .replaceAll(" ", symbols[convention - 1])
+        .replaceAll("_", symbols[convention - 1])
+        .replaceAll("__", symbols[convention - 1])
+        .replaceAll("-", symbols[convention - 1])
+        .replaceAll("--", symbols[convention - 1]);
     }
-    badNamedNodes = []
+    badNamedNodes = [];
     SendToUI({
       action: "SEND_COUNTERS",
       data: {
@@ -221,37 +253,40 @@ figma.ui.onmessage = msg => {
         defaultShapes: defaultShapes.length,
         totalInstances: instancesCount,
         defaultInstances: defaultInstances.length,
-        badNamedNodes: badNamedNodes.length
-      }
-    })
+        badNamedNodes: badNamedNodes.length,
+      },
+    });
   }
 
-  if (action === 'SELECT_ITEM') {
-    const { newName } = msg
+  if (action === "SELECT_ITEM") {
+    const { newName } = msg;
 
     if (newName) {
-      const node = figma.currentPage.findOne((node) => node.id === defaultFrames[0].id)
+      const node = figma.currentPage.findOne(
+        (node) => node.id === defaultFrames[0].id
+      );
       if (node && checkName(node.type, newName)) {
-        node.name = msg.newName
-        defaultFrames.shift()
+        node.name = msg.newName;
+        defaultFrames.shift();
       } else {
-        figma.notify("Node is still have a default name", { error: true, timeout: 3000 })
-        return
+        figma.notify("Node is still have a default name", {
+          error: true,
+          timeout: 3000,
+        });
+        return;
       }
     }
 
     if (defaultFrames.length) {
-      const nextFrame = defaultFrames[0]
+      const nextFrame = defaultFrames[0];
       figma.currentPage.selection = [nextFrame];
-      figma.viewport.scrollAndZoomIntoView([nextFrame])
+      figma.viewport.scrollAndZoomIntoView([nextFrame]);
       SendToUI({
         action: "SEND_NAME",
         data: {
-          name: nextFrame.name
-        }
-        
-      })
+          name: nextFrame.name,
+        },
+      });
     }
-
   }
 };
